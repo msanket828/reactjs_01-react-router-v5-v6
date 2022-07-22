bb16d317d037ab069956123225ebcfd298f28a3a
sanket <sanketm@techved.com>
2022-07-22 23:45:34 +0530

    dynamic routing

--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -1,10 +1,14 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm"
 
-const NewQuotes=(props)=> {
 
+const NewQuotes=(props)=> {
+  const history =useHistory();
   const addQuoteHandler=(quoteData)=> {
     // props.
     console.log(quoteData);
+    history.push('/quotes'); 
+
   }
 
   return <QuoteForm onAddQuote={addQuoteHandler}/>
